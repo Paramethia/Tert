87d126e754c64346574f7e812d16210431e50bdf
Kyrin <kyrinkompi@gmail.com>
2025-09-01 01:05:29 +0200

    Upload files

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Tert
+{
+    partial class Tert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(230, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter your text";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.richTextBox1.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(234, 102);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(506, 218);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(418, 532);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Do it";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(234, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "What you wanna do?";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(234, 396);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 21);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Reverse";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(234, 423);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(63, 21);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Binary";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(234, 451);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(67, 21);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Decode";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(352, 395);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 30);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Visible = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Myanmar Text", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(459, 403);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 21);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Full/each word reversal?";
+            this.label3.Visible = false;
+            // 
+            // Tert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(1000, 700);
+            this.Name = "Tert";
+            this.Text = "Tert";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Tert
+{
+    public partial class Tert : Form
+    {
+        public Tert()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Show textbox
+            textBox1.Visible = true;
+            label3.Visible = true;
+
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            // Hide  first textbox
+            textBox1.Visible = false;
+            label3.Visible = false;
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            label3.Visible = false;
+        }
+
+        string[,,] binaryLetters = new string[2, 26, 2]
+        {
+            {
+                { "A", "01000001" }, { "B", "01000010" }, { "C", "01000011" }, { "D", "01000100" }, { "E", "01000101" }, { "F", "01000110" },
+                { "G", "01000111" }, { "H", "01001000" }, { "I", "01001001" }, { "J", "01001010" }, { "K", "01001011" }, { "L", "01001100" },
+                { "M", "01001101" }, { "N", "01001110" }, { "O", "01001111" }, { "P", "01010000" }, { "Q", "01010001" }, { "R", "01010010" },
+                { "S", "01010011" }, { "T", "01010100" }, { "U", "01010101" }, { "V", "01010110" }, { "W", "01010111" }, { "X", "01011000" },
+                { "Y", "01011001" }, { "Z", "01011010" }
+            },
+            {
+                { "a", "01100001" }, { "b", "01100010" }, { "c", "01100011" }, { "d", "01100100" }, { "e", "01100101" }, { "f", "01100110" },
+                { "g", "01100111" }, { "h", "01101000" }, { "i", "01101001" }, { "j", "01101010" }, { "k", "01101011" }, { "l", "01101100" },
+                { "m", "01101101" }, { "n", "01101110" }, { "o", "01101111" }, { "p", "01110000" }, { "q", "01110001" }, { "r", "01110010" },
+                { "s", "01110011" }, { "t", "01110100" }, { "u", "01110101" }, { "v", "01110110" }, { "w", "01110111" }, { "x", "01111000" },
+                { "y", "01111001" }, { "z", "01111010" }
+            }
+        };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = richTextBox1.Text;
+            bool reverse = radioButton1.Checked;
+            bool binary = radioButton2.Checked;
+            bool decode = radioButton3.Checked;
+
+            if (text != "" && reverse == true)
+            {
+                // Reverse the text
+                uint wordsQuantity = 0U;
+                if (textBox1.Text.ToLower() == "full")
+                {
+                    string totalReversal = new string(text.Reverse().ToArray());
+                    MessageBox.Show("Click ok or close this to copy to clipboard \n \n" + totalReversal, "---Conversion---");
+                    Clipboard.SetText(totalReversal);
+                }
+                else if (textBox1.Text.ToLower() == "each" || textBox1.Text == "")
+                {
+                    string[] words = text.Split(' ');
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        wordsQuantity++;
+                        char[] charArray = words[i].ToCharArray();
+                        Array.Reverse(charArray);
+                        words[i] = new string(charArray);
+                    }
+                    string wordReversal = string.Join(" ", words);
+                    MessageBox.Show("Click ok or close this to copy to clipboard \n \n" + wordReversal + $"\n Amount of words present: {wordsQuantity}", "---Conversion---");
+                    Clipboard.SetText(wordReversal);
+                }
+            }
+            else if (text != "" && binary == true)
+            {
+                // Convert the text to binary
+                string[] words = text.Split(' ');
+                string word;
+
+                List<string> characters = new List<string>();
+                string charac;
+                List<string> binoredChars = new List<string>();
+                string letterCode;
+                string binoredWord = "";
+                string binaryText = "";
+
+                uint wordsQuantity = 0U;
+                for (int i = 0; i < words.Length; i++)
+                {
+                    wordsQuantity++;
+                    word = words[i];
+                    for (int index = 0; index < word.Length; index++)
+                    {
+                        charac = Convert.ToString(word[index]);
+                        characters.Add(charac);
+                        for (int casing = 0; casing < 2; casing++)
+                        {
+                            for (int letter = 0; letter < 26; letter++)
+                            {
+                                string binaryLetter = binaryLetters[casing, letter, 0];
+                                if (binaryLetter == charac)
+                                {
+                                    letterCode = binaryLetters[casing, letter, 1];
+                                    binoredChars.Add(letterCode);
+
+                                    if (binoredChars.Count >= word.Length)
+                                    {
+                                        foreach (string bytee in binoredChars) { binoredWord += bytee; }
+                                        binaryText += binoredWord + " ";
+                                        binoredWord = "";
+                                        binoredChars.Clear();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show("Click ok or close this to copy to clipboard \n \n" + binaryText + $"\n Amount of words present: {wordsQuantity}", "---Conversion---");
+                Clipboard.SetText(binaryText);
+            }
+            else if (text != "" && decode == true && text.Contains("0") && text.Contains("1"))
+            {
+                try
+                {
+                    // Decode the binary to text
+                    string[] binaryWords = text.Split(' ');
+                    string decodedText = "";
+
+                    foreach (string word in binaryWords)
+                    {
+                        for (int i = 0; i < word.Length; i += 8)
+                        {
+                            if (i + 8 <= word.Length)
+                            {
+                                string chunk = word.Substring(i, 8);
+                                try
+                                {
+                                    int ascii = Convert.ToInt32(chunk, 2);
+                                    decodedText += (char)ascii;
+                                }
+                                catch
+                                {
+                                    decodedText += "?";
+                                }
+                            }
+                        }
+                        decodedText += " "; // add a space between words
+                    }
+                    MessageBox.Show("Click ok or close this to copy to clipboard \n \n" + decodedText, "---Conversion---");
+                    Clipboard.SetText(decodedText);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Invalid binary input. Please ensure the input is a valid binary string.", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nothing was converted because the checkbox is not checked or the input is empty.", "Error");
+            }
+            
+        }
+    }
+}
